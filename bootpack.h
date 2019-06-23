685a0f9454adaaefd259c0b3a64e5744a34e199d
fkjslee <245878146@qq.com>
2019-06-23 22:29:15 +0800

    add shutdown by enter

--- bootpack.h
+++ bootpack.h
@@ -263,6 +263,7 @@ void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 struct CONSOLE {
 	struct SHEET *sht;
 	int cur_x, cur_y, cur_c;
+	struct TIMER *timer;
 };
 void console_task(struct SHEET *sheet, unsigned int memtotal);
 void cons_putchar(struct CONSOLE *cons, int chr, char move);
@@ -278,6 +279,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 int* hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
 int* inthandler0d(int *esp);
 int *inthandler0c(int *esp);
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col);
 
 /* file.c */
 struct FILEINFO {
